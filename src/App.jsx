b6536eb04bfc0edfffe18157ea26cbfdc542d7a5
CJay-Cipher <chijiokennabueze@gmail.com>
2025-06-12 14:43:11 +0100

    dashboard update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hone from "./components/Hone";
-
-
-// Import your page components
-import Home from "./components/Home";
-//import About from "./pages/About";
+import HomeScreen from "./pages/Dashboard";
 
 function App() {
   return (
     <div>
-   <Hone />
-     {/*
-    <Router>
-      <Routes>
-        <Route path="/" element={< />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router> */}
+      <HomeScreen />
     </div>
   );
 }
